25949311d2b92f06d397960a92d9e08c84d52efb
Marcos Garcia de Lima <marckfree@gmail.com>
2023-06-26 11:02:46 -0300

    Tests with SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
-import nodejs from 'astrojs/node'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: 'nodejs()',
+  adapter: node({
+    mode: 'standalone'
+  }),
   output: 'hybrid',
 })
